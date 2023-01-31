66e121b7d79d41f196071e6d655f98a5927c89f4
b_jeptum <brendajeptum@gmail.com>
2023-01-31 11:53:23 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#Displays a repeat of character b in string
-puts ARGV[0].scan(/h(b)?tn/).join
+#Displays string with or without character b in string
+puts ARGV[0].scan(/hb?tn/).join
