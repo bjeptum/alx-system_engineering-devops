8d66bdc0dfc53693fa304abcea4b3539c9fe5296
b_jeptum <brendajeptum@gmail.com>
2023-02-01 11:16:59 -0800

    Textme updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 #Matches sender and receiver details(phone number or name) and flags used
-Match = ARGV[0].scan(/from:([^\[]+).*to:([^\[]+).*flags:([^\[]+)/
+Match = ARGV[0].scan(/from:([^\[]+)|to:([^\[]+)|flags:([^\[]+)/
 List = [Match[0].compact, Match[1].compact, Match[2].compact]
 puts List.join(',')
