05d001e03f7406feb5f1f68c985edd0189c8e993
b_jeptum <brendajeptum@gmail.com>
2023-02-01 09:09:56 -0800

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
-# Matches sender and receiver phone number and flags used
-input = "From +123456789: to +987654321: flags [flag1 flag2]"
-regex = /From\s([\+\w]+):\s+to\s([\+\w]+):\s+flags\s(\[.*\])/
-#match the input with the regular expression
-match = input.match(regex)
-#output the results
-puts "[#{match[1]}],[#{match[2]}],[#{match[3]}]"i
+#Matches sender and receiver details(phone number or name) and flags used
+puts ARGV[0].scan(/\b(from|to|flags):([^\]]+)\b/).join
