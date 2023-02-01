3efdf0b1fecae4f8320f5936a16dd7d5830e4a97
b_jeptum <brendajeptum@gmail.com>
2023-02-01 09:16:08 -0800

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #Matches sender and receiver details(phone number or name) and flags used
-puts ARGV[0].scan(/\b(from|to|flags):([^\]]+)\b/).join
+puts ARGV[0].scan(/(?<=(from|to|flags:))[^\]]+/).join
