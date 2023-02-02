c24dc00a39600f0a6a972aff6bec66a53342fa78
b_jeptum <brendajeptum@gmail.com>
2023-02-01 21:34:20 -0800

    Textme updtaed#1

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 #Matches sender and receiver details(phone number or name) and flags used
-Match = ARGV[0].scan(/from:([^\[]+)|to:([^\[]+)|flags:([^\[]+)/
-List = [Match[0].compact, Match[1].compact, Match[2].compact]
-puts List.join(',')
+match = Oniguruma::ORegexp.new(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).match(ARGV[0])
+if match
+  sender = match[1]
+  receiver = match[2]
+  flags = match[3]
+  puts "#{sender},#{receiver},#{flags}"
+else
+  puts "No match found"
+end
