4eb37e6722ba03aaea6a8e665614829b6714a73b
b_jeptum <brendajeptum@gmail.com>
2023-03-28 10:40:05 -0700

    Updated install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,22 +1,5 @@
 # install flask from pip3
-# Install Python
-class { 'python':
-  version        => '3.8.0',
-}
-
-# Install pip
-class { 'python::pip':
-  ensure         => pip3,
-}
-
-# Install requests package
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-}
-
-# Configure Python path
-class { 'python::params':
-  pythonpath     => '/usr/local/bin/python',
-  pip_executable => '/usr/local/bin/pip',
+  provider => 'pip3',
 }
