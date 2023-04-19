808e16cebad673d903be4cb6e58ef11b2ef99cce
b_jeptum <111267046+bjeptum@users.noreply.github.com>
2023-04-19 10:35:55 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,9 +0,0 @@
-# Create a manifest that kills a process named killmenow
-
-exec { 'killmenow':
-  command  => 'pkill killmenow',
-  onlyif   => 'pgrep killmenow',
-  provider => 'shell',
-  creates  => '/killmenow',
-  path     => '/usr/bin',
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,6 +0,0 @@
-##0x0A. Configuration management
-
-
-An introduction to configuration management
-
-This project wil be implementing the Puppet tool
