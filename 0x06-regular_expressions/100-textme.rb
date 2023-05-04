021f8b74bcee1583d1dcb5d505563746b9b2f6cd
root <brendajeptum@gmail.com>
2023-05-03 21:30:37 -0700

    Textme#2

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,5 +2,5 @@
 #Displays [SENDER],[RECEIVER],[FLAGS]
 sender = ARGV[0].scan(/from:(.[\w\s\d\+\(\)]+)/)
 receiver = ARGV[0].scan(/to:(.[\w\s\d\+\(\)]+)/)
-flags = ARGV[0].scan(/flags:(.{12})\d]/)
+flags = ARGV[0].scan(/flags:(.{12})\]/)
 puts [sender, receiver , flags].join(',')
