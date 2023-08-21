87e87632675ea3841ccc8ad39d9e30759d43868f
root <brendajeptum@gmail.com>
2023-08-21 14:07:50 -0700

    Top Ten posts in subreddits

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,26 +13,27 @@ def top_ten(subreddit):
     listed for a given subreddit
     """
     # Reddit API endpoint to get subreddit information
-    url = f"https://www/reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
     # Set a custom User-Agent to avoid Too Many Requests error
     headers = {"User-Agent": "Custom User-Agent"}
 
-    response = requests.get(url, headers=headers,
-                            params={'limit': '10'}, allow_redirects=False)
+    try:
+        query_params = {"limit": 10}
+        response = requests.get(url, headers=headers,
+                                params=query_params, allow_redirects=False)
 
-    if response.status_code == 200:
-        posts = response.json()
+        if response.status_code == 200:
+            posts = response.json()
 
-        # Check if the response contains the posts about the subreddit
-        if "data" in pots and "children" in posts["data"]:
-            for post in posts["data"]["children"]:
-                print(post["data"]["title"])
-        else:
-            print("None")
-
-    else:
-        print("None")
+            # Check if the response contains the posts about the subreddit
+            if "data" in posts and "children" in posts["data"]:
+                for post in posts["data"]["children"]:
+                    print(post["data"]["title"])
+            else:
+                print("None")
+    except requests.exceptions.RequestException:
+        return 0  # Return 0 for any request error
 
 
 if __name__ == "__main__":
