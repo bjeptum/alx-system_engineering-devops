fa433e4c870664bea7eff9a633765ecd0741ebfe
bjeptum <brendajeptum@gmail.com>
2024-06-28 21:05:03 +0000

    Install package using puppet updated#1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install flask version 2.1.0 using Puppet
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
+  require  => Package['python3', 'python3-pip']
 }
