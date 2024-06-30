c9d8b43a4e1f5affcc09e15cd2b1989edf909edb
bjeptum <brendajeptum@gmail.com>
2024-06-30 21:14:18 +0000

    Install flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # Install flask version 2.1.0 using Puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure  => installed,
+ }
+exec { 'install flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
 }
