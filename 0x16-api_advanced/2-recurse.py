c403b5c6bb4cc25b77e699f2cf14ec0689869316
b_jeptum <brendajeptum@gmail.com>
2024-08-15 23:37:17 +0300

    Recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Recursive funtion module
+"""
+import json
+import requests
+import sys
+
+
+def recurse(subreddit, hot_list=None, after=None):
+    """
+    Queries Reddit API and returns
+    a list containing all hot articles from Reddit
+    """
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {"User-Agent": "Custom User-Agent"}
+    query_params = {"after": after}
+
+    response = requests.get(url, headers=headers,
+                            params=query_params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data.get('data', {}).get('subs', [])
+
+        if not subs:
+            return hot_list if hot_list else None
+        for sub in subs:
+            title = sub['data'].get('title')
+            if title:
+                hot_list.append(title)
+
+        # Get the next page of results
+        after = data.get('data', {}).get('after')
+        if after:
+            return recurse(subreddit, hot_list, after)
+
+        return hot_list if hot_list else None
+    else:
+        # Check if an invalid subreddit
+        if response.status_code == 302:
+            return None
+        else:
+            response.raise_for_status()
+
+    if __name__ == "__main__":
+        if len(sys.argv) != 2:
+            print("Usage: python script.py <subreddit>")
+            sys.exit(1)
+
+        subreddit = sys.argv[1]
+        hot_articles = recurse(subreddit)
+        if hot_articles is not None:
+            print(json.dumps(hot_articles, indent=2))
+        else:
+            print("No hot articles found or invalid subreddit.")
